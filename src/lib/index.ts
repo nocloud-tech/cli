081312f9b62aaf7fc8be14b4ddf13f14cb43c530
Michael Ainsworth <michael@michaelainsworth.id.au>
2022-06-28 19:47:16 +1000

    Initial version

---
+++ src/lib/index.ts
@@ -0,0 +1,41 @@
+export type CliFunction = (args:string[]) => Promise<void>;
+
+export type CliFunctionDictionary = {[key:string]:CliFunction};
+
+export type ParameterisedCliFunction = <Parameter>(parameter:Parameter, args:string[]) => Promise<void>
+
+export const CreateParameterisedCliFunction = <Parameter>(creator:() => Promise<Parameter>, fn:(parameter:Parameter, args:string[]) => Promise<void>) : CliFunction => {
+    return async (args:string[]) => {
+        const p = await creator();
+        return await fn(p, args);
+    }
+}
+
+export const Run = async(functions:CliFunctionDictionary, action:"throw"|"exit" = "exit") => {
+    let command:string|undefined;
+
+    try {
+        const args = process.argv.slice(2);
+    
+        if (!args.length) {
+            throw new Error(`Missing command.`);
+        }
+    
+        command = args[0];
+        const fn = functions[command];
+    
+        if (!fn) {
+            throw new Error(`Unknown command '${command}'.`);
+        }
+        await fn(args.slice(1));
+    } catch (error:any) {
+        const message : string = error.message;
+        if ("throw" === action) {
+            throw new Error(message);
+        } else {
+            process.stderr.write(`${message}\n`);
+            process.exit(1);
+        }
+    }
+}
+
---
+++ wiki/README.md
@@ -0,0 +1,9 @@
+# CLI
+
+## Overview
+
+Provides common command-line interface functionality.
+
+## Example Usage
+
+See `cli-example.ts` for an example on how the library is to be used.
