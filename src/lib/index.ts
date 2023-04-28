51ae7e317f1afb2dc58462ef12c7d33f7c630a6a
Michael Ainsworth <michael@michaelainsworth.id.au>
2023-04-28 18:15:07 +1000

    Add argument support

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -83,3 +83,168 @@ export const Run = async(options:RunOptions) => {
     }
 }
 
+export type BooleanArgument = {
+    type: "boolean",
+    boolean?: {
+        defaultValue?: boolean,
+    }
+}
+
+export type NumberArgument = {
+    type: "number"
+    number?: {
+        defaultValue?: number,
+    }
+}
+
+export type StringArgument = {
+    type: "string",
+    string?: {
+        restrict?: string[],
+        defaultValue?: string,
+    }
+}
+
+export type GenericArgument = {
+    short?: string,
+    long?: string,
+    description: string,
+    required: boolean,
+}
+
+export type Argument = GenericArgument & (BooleanArgument|NumberArgument|StringArgument);
+
+export type ArgumentSpecification = {[key:string]:Argument}
+
+export const DescribeArguments = (spec:ArgumentSpecification) => {
+    const cargs = Object.keys(spec)
+
+    const out = (s:string) => {
+        process.stdout.write(s);
+    }
+
+    for (let k = 0; k < cargs.length; ++k) {
+        const property = cargs[k];
+        const carg = spec[property];
+
+        if (carg.short) {
+            out(carg.short);
+            if (carg.long) {
+                out(",");
+            }
+        }
+        if (carg.long) {
+            out(carg.long);
+        }
+        out(` (${carg.type}, ` + (carg.required ? "required" : "optional") + ", ");
+        if (carg.type == "boolean") {
+            if (carg.boolean && carg.boolean.defaultValue) {
+                out("default " + (carg.boolean.defaultValue ? "true" : "false"));
+            } else {
+                out("no default");
+            }
+        } else if (carg.type == "number") {
+            if (carg.number && carg.number.defaultValue) {
+                out("default " + carg.number.defaultValue);
+            } else {
+                out("no default");
+            }
+        } else if (carg.type == "string") {
+            if (carg.string && carg.string.defaultValue) {
+                out("default " + carg.string.defaultValue);
+            } else {
+                out("no default");
+            }
+        } else {
+            throw new Error("Unknown type. This is a programming error.")
+        }
+        out(")\n");
+        out(`  ${carg.description}\n`)
+    }
+}
+
+export const ProcessArguments = <Result extends {[key:string]:any}>(args:string[], spec:ArgumentSpecification) => {
+    let result : {[key:string]:any} = {};
+    const cargs = Object.keys(spec)
+
+    for (let k = 0; k < cargs.length; ++k) {
+        const carg = spec[cargs[k]];
+        if (!carg.short && !carg.long) {
+            throw new Error("Each argument needs a short or long string.")
+        }
+    }
+
+    for (let i = 0; i < args.length; ++i) {
+        const arg = args[i];
+        let found:number|undefined;
+        for (let k = 0; k < cargs.length; ++k) {
+            const carg = spec[cargs[k]];
+
+            if (arg == carg.short || arg == carg.long) {
+                found = k;
+                break;
+            }
+        }
+
+        if ("undefined" == typeof found) {
+            throw new Error(`Unknown argument '${arg}'.`);
+        }
+
+        const property = cargs[found];
+        const carg = spec[property];
+
+        i += 1;
+        if (i >= args.length) {
+            throw new Error(`The ${arg} argument requires a value.`);
+        }
+
+        const value = args[i];
+        if (carg.type == "boolean") {
+            if (-1 !== ["yes", "true", "on", "1"].indexOf(value.toLowerCase())) {
+                result[property] = true;
+            } else if (-1 !== ["no", "false", "off", "0"].indexOf(value.toLowerCase())) {
+                result[property] = false;
+            } else {
+                throw new Error(`The ${arg} argument must be 'yes', 'true', 'on', '1' or 'no', 'false', 'off', '0'.`);
+            }
+        } else if (carg.type == "number") {
+            const f = parseFloat(value);
+            if (isNaN(f)) {
+                throw new Error(`The ${arg} argument must be a number.`);
+            }
+
+            result[property] = f;
+        } else if (carg.type == "string") {
+            if (carg.string && carg.string.restrict && -1 === carg.string.restrict.indexOf(value)) {
+                throw new Error(`The value supplied to the ${arg} argument is not recognised.`);
+            }
+            result[property] = value;
+        } else {
+            throw new Error(`The ${arg} argument had an unknown type. This is a programming error.`);
+        }
+    }
+
+    for (let k = 0; k < cargs.length; ++k) {
+        const property = cargs[0];
+        const carg = spec[property];
+        const name = carg.short || carg.long;
+
+        if (carg.required) {
+            if ("undefined" == typeof result[property]) {
+                throw new Error(`The ${name} argument is required.`);
+            }
+        } else {
+            if ("undefined" == typeof result[property]) {
+                if (carg.type == "boolean" && carg.boolean && "undefined" != typeof carg.boolean.defaultValue) {
+                    result[property] = carg.boolean.defaultValue;
+                } else if (carg.type == "number" && carg.number && "undefined" != typeof carg.number.defaultValue) {
+                    result[property] = carg.number.defaultValue;
+                } else if (carg.type == "string" && carg.string && "undefined" != typeof carg.string.defaultValue) {
+                    result[property] = carg.string.defaultValue;
+                }
+            }
+        }
+    }
+
+    return <Result>result;
+}
